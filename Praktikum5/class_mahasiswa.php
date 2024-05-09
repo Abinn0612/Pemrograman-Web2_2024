9ac0d528ab0c87771b90f7772b8873cacdac6710
Abinnn0612 <batuhirup13@gmail.com>
2024-05-09 10:31:26 +0700

    update

--- Praktikum5/class_mahasiswa.php
+++ Praktikum5/class_mahasiswa.php
@@ -1,5 +1,38 @@
+
 <?php
 class Mahasiswa {
+    // Property
+
+    public $ipk;
+    public $nim;
+    public $nama;
+    public $prodi;
+    public $angkatan;
+
+    // Method
+    function __construct($nim, $nama){
+        $this->nim = $nim;
+        $this->nama = $nama;
+    }
+    function predikat_ipk(){
+        if($this->ipk < 2.0) {
+            return 'CUKUP';
+        } elseif($this->ipk > 2.0 && $this->ipk < 3.0){
+            return 'BAIK';
+        } elseif($this->ipk >= 3.0 && $this->ipk < 3.75){
+            return 'MEMUASKAN';
+        } elseif($this->ipk >= 3.75 && $this->ipk <4.0){
+            return 'CUM LAUDE';
+        }
+    }
+}
+
+// Instance Object
+$mahasiswa1 = new Mahasiswa(223139, 'Tabiin');
+$mahasiswa1->ipk =3.5;
+$mahasiswa1->prodi = "Teknik Informatika";
+
+// class Mahasiswa {
     // Property
 
     public $ipk;
--- Praktikum5/persegiPanjang.php
+++ Praktikum5/persegiPanjang.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 class PersegiPanjang {
@@ -23,4 +24,31 @@ class PersegiPanjang {
 // Instance Object
 $persegi_panjang1 = new PersegiPanjang(10,5);
 echo "Luas Persegi Panjang =" . $persegi_panjang1->getLuas();
+=======
+<?php
+
+class PersegiPanjang {
+    // Property
+    public $panjang;
+    public $lebar;
+
+
+    // Method
+    function __construct($p, $l){
+        $this->panjang = $p;
+        $this->lebar = $l;
+    }
+
+    function getLuas(){
+        return $this->panjang * $this->lebar;
+    }
+    function getKeliling(){
+        return 2 * ($this->panjang + $this->lebar);
+    }
+}
+
+// Instance Object
+$persegi_panjang1 = new PersegiPanjang(10,5);
+echo "Luas Persegi Panjang =" . $persegi_panjang1->getLuas();
+>>>>>>> f216185dd3c1d907508fa382ffce0b747da71452
 echo "<br>Kelling Persegi Panjang =" . $persegi_panjang1->getKeliling();
