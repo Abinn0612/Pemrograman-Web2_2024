86a2ba412aac36a96965ccce71079935a08ac431
Abinnn0612 <batuhirup13@gmail.com>
2024-05-20 15:30:55 +0700

    updated

---
+++ puskesmas-new/resources/views/admin/pasien/index.blade.php
@@ -0,0 +1,27 @@
+<x-layout>
+    <x-slot name="page_name">Halaman Pasien</x-slot>
+    <x-slot name="page_content">
+        <table class="table table-bordered">
+            <tr class="table-success">
+                <td>Id</td>
+                <td>Kode Pasien</td>
+                <td>Nama</td>
+                <td>Tanggal Lahir</td>
+                <td>Gender</td>
+                <td>Email</td>
+                <td>Nama Kelurahan</td>
+            </tr>
+            @foreach ($pasiens as $pasien)
+            <tr>
+                <td>{{ $pasien->id }}</td>
+                <td>{{ $pasien->kode}}</td>
+                <td>{{ $pasien->nama}}</td>
+                <td>{{ $pasien->tgl_lahir}}</td>
+                <td>{{ $pasien->gender}}</td>
+                <td>{{ $pasien->email}}</td>
+                <td>{{ $pasien->kelurahan_nama}}</td>
+            </tr>
+            @endforeach
+        </table>
+    </x-slot>
+</x-layout>
--- puskesmas-new/routes/web.php
+++ puskesmas-new/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Contactcontroller;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\PasienController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,3 +27,7 @@ Route::get('/profil', function () {
 Route::get('/admin2', [Admincontroller::class, 'index']);
 Route::get('/contact', [ContactController::class, 'index']);
 Route::get('/about', [AboutController::class, 'index']);
+
+
+// Praktikum Laravel 3
+Route::get('/admin2/pasien', [PasienController::class, 'index']);
